8f66f48b4958eaf765eeb723fbce565e235ebfc0
Derek Fredrickson <dfreds08@gmail.com>
2024-08-21 22:53:26 -0400

    Implement logic for showing world and client scope

--- src/ts/module.ts
+++ src/ts/module.ts
@@ -2,66 +2,55 @@ import { id as MODULE_ID } from "@static/module.json";
 import { libWrapper } from "@static/lib/shim.ts";
 
 Hooks.once("setup", () => {
-    // Various examples
+    libWrapper.register(
+        MODULE_ID,
+        "SettingsConfig.prototype._prepareCategoryData",
+        function (
+            this: SettingsConfig,
+            wrapped: () => CategoryData,
+        ): CategoryData {
+            const result = wrapped();
 
-    libWrapper.register(
-        MODULE_ID,
-        "Canvas.prototype._onDrop",
-        function (this: any, wrapped: AnyFunction, ...args: any) {
-            console.log(this);
-            wrapped(...args);
-        },
-    );
-    libWrapper.register(
-        MODULE_ID,
-        "Application.prototype.bringToTop",
-        function (this: Application, wrapped: () => void) {
-            wrapped();
-        },
-    );
-    libWrapper.register(
-        MODULE_ID,
-        "Application.prototype.minimize",
-        function (this: Application, wrapped: () => Promise<boolean>) {
-            const r = wrapped();
-            r.then(() => console.log("yay"));
-            return r;
-        },
-    );
-    libWrapper.register(
-        MODULE_ID,
-        "Token.prototype._canDrag",
-        function (this: any, wrapped: AnyFunction, ...args: any) {
-            return wrapped(...args);
-        },
-        "WRAPPER",
-    );
-    libWrapper.register(
-        MODULE_ID,
-        "Token.prototype._onDragLeftStart",
-        async function (
-            this: Token,
-            wrapped: (event: any) => any,
-            event: Event,
-        ) {
-            const result = wrapped(event);
+            result.categories = result.categories.map((category: Category) => {
+                category.menus = category.menus.map((menu: MenuData) => {
+                    const localized = game.i18n.localize(menu.name);
+                    menu.name = `🌎 ${localized}`;
+                    return menu;
+                });
+
+                category.settings = category.settings.map(
+                    (setting: SettingData) => {
+                        if (setting.scope === "world") {
+                            setting.name = `🌎 ${setting.name}`;
+                        } else {
+                            setting.name = `👤 ${setting.name}`;
+                        }
+
+                        return setting;
+                    },
+                );
+
+                return category;
+            });
             return result;
         },
-        "WRAPPER",
-    );
-    // Playlist overrides
-    libWrapper.register(
-        MODULE_ID,
-        "Playlist.prototype._onSoundStart",
-        _onSoundStartWrapper,
-        "WRAPPER",
     );
 });
 
-export async function _onSoundStartWrapper(
-    this: Playlist,
-    wrapped: (sound: PlaylistSound<null>) => void,
-    sound: PlaylistSound<null>,
-): Promise<void> {
-    wrapped(sound);
-}
+type CategoryData = {
+    categories: Category[];
+};
+
+type Category = {
+    menus: MenuData[];
+    settings: SettingData[];
+};
+
+type MenuData = {
+    name: string;
+};
+
+type SettingData = {
+    name: string;
+    scope: string;
+};
