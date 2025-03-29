8d44569feea65afabf4662eab6fd14fa4806bd40
Derek Fredrickson <dfreds08@gmail.com>
2025-03-29 16:01:45 -0400

    Fix logic and change icons from emoji to font awesome

--- src/ts/hooks/renderSettingsConfig.ts
+++ src/ts/hooks/renderSettingsConfig.ts
@@ -1,19 +1,65 @@
 import { Listener } from "./index.ts";
+import { Settings } from "../settings.ts";
 
 const RenderSettingsConfig: Listener = {
     listen(): void {
         Hooks.on(
             "renderSettingsConfig",
-            (_config: any, _html: any, _data: any) => {
-                // const moduleSettings = new Settings();
+            (_config: any, html: any, _data: any) => {
+                const moduleSettings = new Settings();
+                const $html = $(html);
 
                 for (const menu of game.settings.menus.values()) {
-                    console.log("menu", menu);
+                    const section = findSectionForSetting({
+                        $html,
+                        namespace: menu.namespace,
+                    });
+                    if (!section) continue;
+
+                    const menuLabel = findMenuLabel({
+                        section,
+                        key: menu.key,
+                    });
+                    if (!menuLabel) continue;
+
+                    addIconToMenuLabel({
+                        isWorld: menu.restricted,
+                        label: menuLabel,
+                    });
                 }
 
                 for (const setting of game.settings.settings.values()) {
-                    console.log("setting", setting);
-                    break;
+                    const section = findSectionForSetting({
+                        $html,
+                        namespace: setting.namespace,
+                    });
+                    if (!section) continue;
+
+                    const settingLabel = findSettingLabel({
+                        section,
+                        identifier: setting.id ?? "",
+                    });
+                    if (!settingLabel) continue;
+
+                    addIconToSettingLabel({
+                        isWorld: setting.scope === "world",
+                        label: settingLabel,
+                    });
+
+                    if (moduleSettings.showNonDefaultIndicator) {
+                        const settingValue = game.settings.get(
+                            setting.namespace,
+                            setting.key,
+                        );
+
+                        toggleChangedIndicator({
+                            identifier: setting.id ?? "",
+                            original: setting.default,
+                            value: settingValue,
+                            categorySection: section,
+                            choices: setting.choices,
+                        });
+                    }
                 }
 
                 // const settingsData = data as SettingsData;
@@ -41,12 +87,6 @@ const RenderSettingsConfig: Listener = {
                 //             (setting: SettingData) => {
                 //                 setting.name = game.i18n.localize(setting.name);
 
-                //                 addIconToLabel({
-                //                     identifier: setting.id,
-                //                     isWorld: setting.scope === "world",
-                //                     categorySection: section,
-                //                 });
-
                 //                 if (moduleSettings.showNonDefaultIndicator) {
                 //                     toggleChangedIndicator({
                 //                         identifier: setting.id,
@@ -68,20 +108,39 @@ const RenderSettingsConfig: Listener = {
     },
 };
 
+function findSectionForSetting({
+    $html,
+    namespace,
+}: {
+    $html: JQuery<HTMLElement>;
+    namespace: string;
+}): JQuery<HTMLElement> | null {
+    const section = $html.find(
+        `.categories section[data-category="${namespace}"]`,
+    );
+
+    return section;
+}
+
 function toggleChangedIndicator({
     identifier,
     original,
     value,
     categorySection,
+    choices,
 }: {
     identifier: string;
     original: any;
     value: any;
     categorySection: JQuery<HTMLElement>;
+    choices?: Record<string, unknown>;
 }) {
     const formGroup = categorySection
-        .find(`[name="${identifier}"], [data-key="${identifier}"]`)
+        .find(`[name="${identifier}"]`)
         .closest(".form-group");
+
+    if (!formGroup.length) return;
+
     const notes = formGroup.find("p");
 
     // Fixes issue with some settings being null by default (why tho)
@@ -92,25 +151,81 @@ function toggleChangedIndicator({
             formGroup.removeClass("obvious-settings-modified");
         } else {
             formGroup.addClass("obvious-settings-modified");
-            notes.append(`<p><b>Default</b>: ${original}`);
+
+            if (choices) {
+                const originalChoice = choices[original];
+
+                if (originalChoice) {
+                    notes.append(
+                        `<p><b>Default</b>: ${game.i18n.localize(
+                            originalChoice as string,
+                        )}</p>`,
+                    );
+                } else {
+                    notes.append(`<p><b>Default</b>: ${original}</p>`);
+                }
+            } else {
+                notes.append(`<p><b>Default</b>: ${original}</p>`);
+            }
         }
     }
 }
 
-function addIconToLabel({
+function findSettingLabel({
+    section,
     identifier,
-    isWorld,
-    categorySection,
 }: {
+    section: JQuery<HTMLElement>;
     identifier: string;
-    isWorld: boolean;
-    categorySection: JQuery<HTMLElement>;
-}) {
-    const icon = isWorld ? "🌎" : "👤";
-    const label = categorySection
-        .find(`[name="${identifier}"], [data-key="${identifier}"]`)
+}): JQuery<HTMLElement> | null {
+    const label = section
+        .find(`[name="${identifier}"]`)
         .closest(".form-group")
         .find("label");
+
+    return label;
+}
+
+function addIconToSettingLabel({
+    isWorld,
+    label,
+}: {
+    isWorld: boolean;
+    label: JQuery<HTMLElement>;
+}) {
+    const icon = isWorld
+        ? "<i class='fas fa-globe'></i>"
+        : "<i class='fas fa-user'></i>";
+
+    label.prepend(`${icon} `);
+}
+
+function findMenuLabel({
+    section,
+    key,
+}: {
+    section: JQuery<HTMLElement>;
+    key: string;
+}): JQuery<HTMLElement> | null {
+    const label = section
+        .find(`button[data-key="${key}"]`)
+        .closest(".form-group")
+        .find("label");
+
+    return label;
+}
+
+function addIconToMenuLabel({
+    isWorld,
+    label,
+}: {
+    isWorld: boolean;
+    label: JQuery<HTMLElement>;
+}) {
+    const icon = isWorld
+        ? "<i class='fas fa-globe'></i>"
+        : "<i class='fas fa-user'></i>";
+
     label.prepend(`${icon} `);
 }
 
