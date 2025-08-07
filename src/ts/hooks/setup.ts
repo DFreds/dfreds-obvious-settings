e2e2a14ba7c67680009cab318bc1390b107060ae
Derek Fredrickson <dfreds08@gmail.com>
2025-08-07 16:49:12 -0400

    Simplify debug hook logic

--- src/ts/hooks/setup.ts
+++ src/ts/hooks/setup.ts
@@ -3,10 +3,7 @@ import { Listener } from "./index.ts";
 const Setup: Listener = {
     listen(): void {
         Hooks.once("setup", () => {
-            if (BUILD_MODE === "development") {
-                // Setup debug mode
-                CONFIG.debug.hooks = true;
-            }
+            CONFIG.debug.hooks = BUILD_MODE === "development";
         });
     },
 };
